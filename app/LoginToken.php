6fc789d334b7debf4e50b4d459e18e45c9c94148
Amir Mehrabi <aut0run2011@gmail.com>
2018-12-21 20:27:28 +0330

    added generateFor method to LoginToken

--- app/LoginToken.php
+++ app/LoginToken.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class LoginToken extends Model
 {
-    //
+    public function generateFor(User $user)
+    {
+      return static::create([
+        'user_id' => $user->id,
+        'token' => str_random(50)
+      ]);
+    }
 }
